4e497191fc2d0538efca825cf437fbf9e4e2f2be
skumari1 <skumari1@in.ibm.com>
2020-09-30 17:23:12 +0530

    Initial Commiy

---
+++ app/src/main/java/com/android/sweta/motionlayoutsample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.android.sweta.motionlayoutsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
